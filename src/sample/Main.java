9e1ba15fe19243b0006dfcb33be606a4a465f01a
Danylo <danyloboshagora@gmail.com>
2020-11-03 09:30:28 +0100

    Added sout output score.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,8 @@
 package sample;
 
+import be.Movie;
+import be.Review;
+import be.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +13,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // primaryStage.setTitle("Hello World");
+        // primaryStage.setScene(new Scene(root, 300, 275));
+        // primaryStage.show();
+
+        User u1 = new User(1, "Bill", 23);
+        Movie m1 = new Movie("Tenet", "Robert Patison");
+        Review r1 = new Review(u1.getUserId(),m1.getMovieTtle());
+        r1.setScore(5);
+        System.out.println(r1.getScore());
+
+
     }
 
 
